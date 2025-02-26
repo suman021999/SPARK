ec2053b95b79014818393720f58c4ad12ec9cf5d
Shank34 <spikbang@gmail.com>
2025-02-26 13:11:41 +0530

    description

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import database from "./db/database.js"
 
 const app=express()
+dotenv.config()
+database()
+
+app.use(cors({
+    credentials:true
+}))
+
+const port=process.env.PORT||3000
+
+app.listen(console.log(`server run on ${port}`))
