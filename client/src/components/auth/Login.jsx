b992777424ced8ab6f45c6afa8357503185720fc
Shank34 <spikbang@gmail.com>
2025-04-20 18:46:53 +0530

    description

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -16,7 +16,7 @@ const{username, setUsername}=useContext(PhoneContext)
   const handleLogin=async(evx)=>{
     evx.preventDefault();
     try {
-      const res=await axios.post(`${import.meta.env.VITE_AUTH_URL}/login`,{
+      const res=await axios.post(`${import.meta.env.VITE_AUTH_URL}/api/v1/auth/login`,{
         username,
         password,
       })
