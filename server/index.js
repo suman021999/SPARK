df5caba187356433a4570fd6295a6f66894a7965
Shank34 <spikbang@gmail.com>
2025-06-29 10:24:54 +0530

    description

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ database();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:8000",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
